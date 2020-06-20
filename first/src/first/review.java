9a1f0d4e1ba0580e4bab322e992f464c3e75feb0
dbdmsdn10 <dbdmsdn10@gmail.com>
2020-06-21 00:34:33 +0900

    배포용 1.3
    과거보기, 가장 가까운 과거부터 보게함
    choice사용해 년도, 월, 날짜 설정할수 있게함
    이것을 사용해 시작지점, 끝지점을 정해 그 사이의 날짜만 보이게 할수있음

--- first/src/first/review.java
+++ first/src/first/review.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -11,6 +13,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 
 import javax.swing.JFrame;
@@ -27,6 +30,10 @@ import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JToolBar;
+import java.awt.Choice;
 
 public class review {
 
@@ -36,10 +43,19 @@ public class review {
 	private JTextField penalty;
 	JList daylist, morninglist, lunchlist, dinnerlist, snacklist, worklist;
 	String daylist2[] = null;
+	static String daylist3[] = null;
 	String Food[][];
 	String mettable[][];
 	long[] savelist = null;
 	JButton back = new JButton("돌아가기");
+	static Choice startyear = new Choice();
+	static Choice startmon = new Choice();
+	static Choice startday = new Choice();
+	JButton daycar = new JButton("날짜 계산");
+
+	static Choice endyear = new Choice();
+	static Choice endmon = new Choice();
+	static Choice endday = new Choice();
 
 	/**
 	 * Launch the application.
@@ -49,10 +65,15 @@ public class review {
 	 * Create the frame.
 	 */
 	public review() {
-
+		startyear.removeAll();
+		startmon.removeAll();
+		startday.removeAll();
+		endyear.removeAll();
+		endmon.removeAll();
+		endday.removeAll();
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setBounds(100, 100, 643, 470);
+		contentPane.setBounds(100, 100, 670, 470);
 
 		contentPane.setLayout(null);
 
@@ -152,10 +173,62 @@ public class review {
 		penalty.setBounds(429, 380, 116, 21);
 		contentPane.add(penalty);
 
-		
 		back.setBounds(530, 10, 97, 23);
 		contentPane.add(back);
+
+		JLabel lblNewLabel_4 = new JLabel("시작기간");
+		lblNewLabel_4.setBounds(561, 57, 57, 15);
+		contentPane.add(lblNewLabel_4);
+
+		startyear.setBounds(561, 78, 63, 21);
+		contentPane.add(startyear);
+
+		startmon.setBounds(561, 105, 63, 21);
+		contentPane.add(startmon);
+
+		startday.setBounds(561, 132, 63, 21);
+
+		daycar.setBounds(561, 379, 97, 23);
+		contentPane.add(daycar);
+		contentPane.add(startday);
+
+		endyear.setBounds(561, 218, 63, 21);
+		contentPane.add(endyear);
+
+		endmon.setBounds(561, 241, 63, 21);
+		contentPane.add(endmon);
+
+		endday.setBounds(561, 268, 63, 21);
+		contentPane.add(endday);
+
+		JLabel lblNewLabel_5 = new JLabel("종료기간");
+		lblNewLabel_5.setBounds(561, 197, 57, 15);
+		contentPane.add(lblNewLabel_5);
 		
+		JLabel lblNewLabel_6 = new JLabel("년도");
+		lblNewLabel_6.setBounds(632, 83, 26, 15);
+		contentPane.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("월");
+		lblNewLabel_7.setBounds(630, 111, 28, 15);
+		contentPane.add(lblNewLabel_7);
+		
+		JLabel lblNewLabel_8 = new JLabel("일");
+		lblNewLabel_8.setBounds(635, 138, 23, 15);
+		contentPane.add(lblNewLabel_8);
+		
+		JLabel lblNewLabel_9 = new JLabel("년도");
+		lblNewLabel_9.setBounds(630, 218, 28, 15);
+		contentPane.add(lblNewLabel_9);
+		
+		JLabel lblNewLabel_10 = new JLabel("월");
+		lblNewLabel_10.setBounds(630, 245, 28, 15);
+		contentPane.add(lblNewLabel_10);
+		
+		JLabel lblNewLabel_11 = new JLabel("일");
+		lblNewLabel_11.setBounds(630, 274, 28, 15);
+		contentPane.add(lblNewLabel_11);
+
 	}
 
 	public void get(String[][] Food, String mettable[][]) {
@@ -164,91 +237,179 @@ public class review {
 		savelist = new long[mettable.length];
 	}
 
-	public static void carcul(review frame,mainframe frame2) {
-		SimpleDateFormat turn = new SimpleDateFormat("yyyy MM dd");
-		SimpleDateFormat year = new SimpleDateFormat("yyyy");
-		SimpleDateFormat mon = new SimpleDateFormat("MM");
-		SimpleDateFormat day = new SimpleDateFormat("dd");
+	public static void carcul(review frame, mainframe frame2) {
+		
 
 		Calendar cal = Calendar.getInstance();
 
 		frame.back.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JPanel panel=frame2.Panel();
+				JPanel panel = frame2.Panel();
 				frame2.setContentPane(panel);
 				frame2.setBounds(100, 100, panel.getWidth(), panel.getHeight());
 				frame2.search(frame2);
-				
+
 			}
 		});
-		
-		
-		
-		
-		cal.add(Calendar.DAY_OF_MONTH, -365);
+
 		File year2 = new File("date");
 		ArrayList<String> daylistmodel = new ArrayList<String>();
-
+		Calendar cal2 = (Calendar) cal.clone();
+		cal2.add(Calendar.DAY_OF_MONTH, -365);
 		if (year2.isDirectory()) {
-			for (int i = 0; i < 365; i++) {
-				String time4 = year.format(cal.getTime());
-
-				File year3 = new File("date/" + time4);
-				// --------------------------
-				if (year3.isDirectory()) {// 연도 확인
-					String time5 = mon.format(cal.getTime());
-					File year4 = new File("date/" + time4 + "/" + time5);
-					if (year4.isDirectory()) {// 달 확인
-						String time6 = day.format(cal.getTime());
-						File year5 = new File("date/" + time4 + "/" + time5 + "/" + time6);
-
-						if (year5.isDirectory()) {// 일확인
-
-							daylistmodel.add(year.format(cal.getTime()) + "/" + mon.format(cal.getTime()) + "/"
-									+ day.format(cal.getTime()));
-						} else {
-
-						}
-					} else {
-						Calendar time = Calendar.getInstance();
-						time.set(Integer.parseInt(year.format(cal.getTime())),
-								Integer.parseInt(mon.format(cal.getTime())), 1);
-						long miner = time.getTime().getTime() - cal.getTime().getTime();
-						int result = (int) (miner / (24 * 60 * 60 * 1000));
-						cal.add(Calendar.DAY_OF_MONTH, +result);
-						i += result;
-
-						cal.add(Calendar.DAY_OF_MONTH, -1);
-						i--;
-					}
-
-				}
-				// ---------------------------------
-				else {
-					Calendar time = Calendar.getInstance();
-					time.set(Integer.parseInt(year.format(cal.getTime())) + 1, 0, 1);
-					long miner = time.getTime().getTime() - cal.getTime().getTime();
-					int result = (int) (miner / (24 * 60 * 60 * 1000));
-					cal.add(Calendar.DAY_OF_MONTH, +result);
-					i += result;
-					cal.add(Calendar.DAY_OF_MONTH, -1);
-					i--;
-				}
-				cal.add(Calendar.DAY_OF_MONTH, +1);
-			}
+			daylistmodel = search(cal2, cal);
 		} else {
 
 		}
 		frame.daylist2 = daylistmodel.toArray(new String[daylistmodel.size()]);
-		DefaultListModel model = new DefaultListModel();
 
+		DefaultListModel model = new DefaultListModel();
+		int[] startset = new int[3];// 0년도 1월 2날짜
+		int[] nowset = new int[3];
+		boolean differyear = true;
+		boolean differday = true;
+		ArrayList<Integer> daylist = new ArrayList<Integer>();
+		ArrayList<Integer> monlist = new ArrayList<Integer>();
 		for (int i = 0; i < frame.daylist2.length; i++) {
+			String a[] = frame.daylist2[i].split("/");
+
+			if (i == 0) {
+				nowset[0] = Integer.parseInt(a[0]);
+				nowset[1] = Integer.parseInt(a[1]);
+				monlist.add(Integer.parseInt(a[1]));
+				frame.startyear.add(a[0]);
+				frame.endyear.add(a[0]);
+
+			}
+			if (differyear && nowset[0] != Integer.parseInt(a[0])) {
+				daylist.removeAll(daylist);
+				nowset[0] = Integer.parseInt(a[0]);
+				frame.startyear.add(a[0]);
+				frame.endyear.add(a[0]);
+				frame.startyear.select(1);
+				if (differyear) {
+
+					Collections.sort(monlist);
+					Integer[] monlist2 = monlist.toArray(new Integer[monlist.size()]);
+					monlist.removeAll(monlist);
+					for (int j = 0; j < monlist2.length; j++) {
+						String q = Integer.toString(monlist2[j]);
+						if (monlist2[j] < 10) {
+							q = "0" + Integer.toString(monlist2[j]);
+						}
+						frame.endmon.add(q);
+					}
+
+					frame.endmon.select(monlist2.length - 1);
+				}
+				differyear = false;
+			}
+			if (differyear && nowset[1] != Integer.parseInt(a[1])) {
+				
+				nowset[1] = Integer.parseInt(a[1]);
+				monlist.add(Integer.parseInt(a[1]));
+				if (differday) {
+					Collections.sort(daylist);
+					Integer[] daylist2 = daylist.toArray(new Integer[daylist.size()]);
+					daylist.removeAll(daylist);
+					for (int j = 0; j < daylist2.length; j++) {
+						String q = Integer.toString(daylist2[j]);
+						if (daylist2[j] < 10) {
+							q = "0" + Integer.toString(daylist2[j]);
+						}
+
+						frame.endday.add(q);
+					}
+					frame.endday.select(daylist2.length - 1);
+					
+				}
+				daylist.removeAll(daylist);
+				differday = false;
+			}
+			if (differday) {
+				daylist.add(Integer.parseInt(a[2]));
+			}
+			// -----------------------------------------------
+			if (!differyear && nowset[1] != Integer.parseInt(a[1])) {
+				nowset[1] = Integer.parseInt(a[1]);
+				monlist.add(Integer.parseInt(a[1]));
+				daylist.removeAll(daylist);
+			}
+			if (!differday) {
+				daylist.add(Integer.parseInt(a[2]));
+			}
+			if (i == frame.daylist2.length - 1) {
+				Collections.sort(monlist);
+				Integer[] monlist2 = monlist.toArray(new Integer[monlist.size()]);
+				monlist.removeAll(monlist);
+				for (int j = 0; j < monlist2.length; j++) {
+					String q = Integer.toString(monlist2[j]);
+					if (monlist2[j] < 10) {
+						q = "0" + Integer.toString(monlist2[j]);
+					}
+					frame.startmon.add(q);
+					if(differyear) {
+						frame.endmon.add(q);
+					}
+				}
+				if(differyear) {
+					frame.endmon.select(monlist2.length-1);
+				}
+				Collections.sort(daylist);
+				Integer[] daylist2 = daylist.toArray(new Integer[daylist.size()]);
+				daylist.removeAll(daylist);
+				for (int j = 0; j < daylist2.length; j++) {
+					String q = Integer.toString(daylist2[j]);
+					if (daylist2[j] < 10) {
+						q = "0" + Integer.toString(daylist2[j]);
+					}
+
+					frame.startday.add(q);
+					
+				}
+			}
 			model.addElement(frame.daylist2[i]);
 		}
+
 		frame.daylist.setModel(model);
 		aaa select = new aaa();
 		select.get(frame);
 		frame.daylist.addListSelectionListener(select);
+		// ---------------------------------------------------
+		frame.daycar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				ArrayList<String> daylistmodel = new ArrayList<String>();
+				Calendar cal2 = (Calendar) cal.clone();
+
+				cal2.set(Integer.parseInt(frame.startyear.getSelectedItem()),
+						Integer.parseInt(frame.startmon.getSelectedItem()) - 1,
+						Integer.parseInt(frame.startday.getSelectedItem()));
+				Calendar cal3 = Calendar.getInstance();
+				cal3.set(Integer.parseInt(frame.endyear.getSelectedItem()),
+						Integer.parseInt(frame.endmon.getSelectedItem()) - 1,
+						Integer.parseInt(frame.endday.getSelectedItem()));
+				if (year2.isDirectory()) {
+					daylistmodel = search(cal2, cal3);
+				} else {
+
+				}
+				frame.daylist2 = daylistmodel.toArray(new String[daylistmodel.size()]);
+				DefaultListModel model2 = new DefaultListModel();
+
+				for (int i = 0; i < frame.daylist2.length; i++) {
+					model2.addElement(frame.daylist2[i]);
+				}
+				frame.daylist.setModel(model2);
+
+			}
+		});
+		frame.daylist3 = frame.daylist2.clone();
+		ChoiceEventHandler choice = new ChoiceEventHandler(frame, frame.daylist3);
+		startyear.addItemListener(choice);
+		startmon.addItemListener(choice);
+		endyear.addItemListener(choice);
+		endmon.addItemListener(choice);
 	}
 
 	public void OpenFile(String file, review frame) {
@@ -355,7 +516,7 @@ public class review {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//-----------------------------------------------
+		// -----------------------------------------------
 		try {
 			File file2 = new File("date/" + file + "/totalkcal.txt");
 			BufferedReader met3 = new BufferedReader(new FileReader(file2));
@@ -363,10 +524,10 @@ public class review {
 			String a = null;
 			ArrayList<String[]> mettablearray = new ArrayList<String[]>();
 			while ((line = met3.readLine()) != null) {
-				a=line;
+				a = line;
 			}
 
-			frame.totaleat.setText(a+" kcal");
+			frame.totaleat.setText(a + " kcal");
 		} catch (IOException q) {// 파일 읽기 오류
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -378,10 +539,10 @@ public class review {
 			String a = null;
 			ArrayList<String[]> mettablearray = new ArrayList<String[]>();
 			while ((line = met3.readLine()) != null) {
-				a=line;
+				a = line;
 			}
 
-			frame.totalwork.setText(a+" kcal");
+			frame.totalwork.setText(a + " kcal");
 		} catch (IOException q) {// 파일 읽기 오류
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -394,10 +555,10 @@ public class review {
 			String a = null;
 			ArrayList<String[]> mettablearray = new ArrayList<String[]>();
 			while ((line = met3.readLine()) != null) {
-				a=line;
+				a = line;
 			}
 
-			frame.penalty.setText(a+" kcal");
+			frame.penalty.setText(a + " kcal");
 		} catch (IOException q) {// 파일 읽기 오류
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -417,6 +578,61 @@ public class review {
 
 		return String.format("%02d : %02d : %02d : %02d", h, m, s, ms);
 	}
+
+	public static ArrayList<String> search(Calendar start, Calendar end) {
+		SimpleDateFormat turn = new SimpleDateFormat("yyyy MM dd");
+		SimpleDateFormat year = new SimpleDateFormat("yyyy");
+		SimpleDateFormat mon = new SimpleDateFormat("MM");
+		SimpleDateFormat day = new SimpleDateFormat("dd");
+		ArrayList<String> daylistmodel = new ArrayList<String>();
+		long miner = end.getTime().getTime() - start.getTime().getTime();
+		int resul2 = (int) (miner / (24 * 60 * 60 * 1000));
+		for (int i = 0; i < resul2; i++) {
+			
+			end.add(Calendar.DAY_OF_MONTH, -1);
+
+			String time4 = year.format(end.getTime());
+
+			File year3 = new File("date/" + time4);
+			// --------------------------
+			if (year3.isDirectory()) {// 연도 확인
+				String time5 = mon.format(end.getTime());
+				File year4 = new File("date/" + time4 + "/" + time5);
+				if (year4.isDirectory()) {// 달 확인
+					String time6 = day.format(end.getTime());
+					File year5 = new File("date/" + time4 + "/" + time5 + "/" + time6);
+
+					if (year5.isDirectory()) {// 일확인
+						daylistmodel.add(year.format(end.getTime()) + "/" + mon.format(end.getTime()) + "/"
+								+ day.format(end.getTime()));
+
+					} else {
+
+					}
+				} else {
+					Calendar time = Calendar.getInstance();
+					time.set(Integer.parseInt(year.format(end.getTime())), Integer.parseInt(mon.format(end.getTime()))-1,
+							1);
+					long miner2 = end.getTime().getTime() - time.getTime().getTime();
+					int result = (int) (miner2 / (24 * 60 * 60 * 1000));
+					end.add(Calendar.DAY_OF_MONTH, -result);
+					i += result;
+				}
+			}
+			// ---------------------------------
+			else {
+				Calendar time = Calendar.getInstance();
+				time.set(Integer.parseInt(year.format(end.getTime())), 0, 1);
+				long miner2 = end.getTime().getTime() - time.getTime().getTime();
+				int result = (int) (miner2 / (24 * 60 * 60 * 1000));
+				end.add(Calendar.DAY_OF_MONTH, -result);
+				i += result;
+
+			}
+		}
+		return daylistmodel;
+
+	}
 }
 
 class aaa implements ListSelectionListener {
@@ -429,8 +645,163 @@ class aaa implements ListSelectionListener {
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		if (re.daylist.getSelectedIndex() != -1) {
+			
 			re.OpenFile(re.daylist2[re.daylist.getSelectedIndex()], re);
 		}
+
+	}
+}
+
+class ChoiceEventHandler implements ItemListener {
+
+	review frame;
+	String daylist3[];
+
+	ChoiceEventHandler(review frame, String daylist3[]) {
+		this.frame = frame;
+		this.daylist3 = daylist3;
+
+	}
+
+	public void itemStateChanged(ItemEvent e) {
+		if (e.getSource() == frame.startyear) {
+			int a = 0;
+			ArrayList<Integer> monlist = new ArrayList<Integer>();
+			ArrayList<Integer> daylist = new ArrayList<Integer>();
+			boolean day = false;
+			for (int i = daylist3.length - 1; i > 0; i--) {
+				String b[];
+				b = daylist3[i].split("/");
+				Choice choice = (Choice) e.getSource();
+				if (b[0].equals(choice.getSelectedItem()) && a != Integer.parseInt(b[1])) {
+					a = Integer.parseInt(b[1]);
+					monlist.add(Integer.parseInt(b[1]));
+					if (monlist.size() == 1) {
+						day = true;
+					} else {
+						day = false;
+					}
+				}
+				if (day) {
+					daylist.add(Integer.parseInt(b[2]));
+				}
+			}
+			Collections.sort(monlist);
+			Integer[] monlist2 = monlist.toArray(new Integer[monlist.size()]);
+			frame.startmon.removeAll();
+			for (int j = 0; j < monlist2.length; j++) {
+				String q = Integer.toString(monlist2[j]);
+				if (monlist2[j] < 10) {
+					q = "0" + Integer.toString(monlist2[j]);
+				}
+				frame.startmon.add(q);
+
+			}
+			Integer[] daylist2 = daylist.toArray(new Integer[daylist.size()]);
+			frame.startday.removeAll();
+			for (int j = 0; j < daylist2.length; j++) {
+				String q = Integer.toString(daylist2[j]);
+				if (daylist2[j] < 10) {
+					q = "0" + Integer.toString(daylist2[j]);
+				}
+				frame.startday.add(q);
+
+			}
+			
+		}
+		if (e.getSource() == frame.startmon) {
+
+			int a = 0;
+			ArrayList<Integer> daylist = new ArrayList<Integer>();
+			boolean day = false;
+			for (int i = daylist3.length - 1; i > 0; i--) {
+				String b[];
+				b = daylist3[i].split("/");
+				Choice choice = (Choice) e.getSource();
+
+				if (b[0].equals(frame.startyear.getSelectedItem()) && (choice.getSelectedItem()).equals(b[1])) {
+					daylist.add(Integer.parseInt(b[2]));
+
+				}
+			}
+			Integer[] daylist2 = daylist.toArray(new Integer[daylist.size()]);
+			frame.startday.removeAll();
+			for (int j = 0; j < daylist2.length; j++) {
+				frame.startday.add(Integer.toString(daylist2[j]));
+			}
+		}
+
+//----------------------------------------------------end
+		if (e.getSource() == frame.endyear) {
+			int a = 0;
+			ArrayList<Integer> monlist = new ArrayList<Integer>();
+			ArrayList<Integer> daylist = new ArrayList<Integer>();
+			boolean day = false;
+			for (int i = daylist3.length - 1; i > 0; i--) {
+				String b[];
+				b = daylist3[i].split("/");
+				Choice choice = (Choice) e.getSource();
+				if (b[0].equals(choice.getSelectedItem()) && a != Integer.parseInt(b[1])) {
+					a = Integer.parseInt(b[1]);
+					monlist.add(Integer.parseInt(b[1]));
+					if (monlist.size() == 1) {
+						day = true;
+					} else {
+						day = false;
+					}
+				}
+				if (day) {
+					daylist.add(Integer.parseInt(b[2]));
+				}
+			}
+			Collections.sort(monlist);
+			Integer[] monlist2 = monlist.toArray(new Integer[monlist.size()]);
+			frame.endmon.removeAll();
+			for (int j = 0; j < monlist2.length; j++) {
+				String q = Integer.toString(monlist2[j]);
+				if (monlist2[j] < 10) {
+					q = "0" + Integer.toString(monlist2[j]);
+				}
+				frame.endmon.add(q);
+
+			}
+			Integer[] daylist2 = daylist.toArray(new Integer[daylist.size()]);
+			frame.endday.removeAll();
+			for (int j = 0; j < daylist2.length; j++) {
+				String q = Integer.toString(daylist2[j]);
+				if (daylist2[j] < 10) {
+					q = "0" + Integer.toString(daylist2[j]);
+				}
+				frame.endday.add(q);
+
+			}
+			
+		}
+		if (e.getSource() == frame.endmon) {
+
+			int a = 0;
+			ArrayList<Integer> daylist = new ArrayList<Integer>();
+			boolean day = false;
+			for (int i = daylist3.length - 1; i > 0; i--) {
+				String b[];
+				b = daylist3[i].split("/");
+				Choice choice = (Choice) e.getSource();
+
+				if (b[0].equals(frame.endyear.getSelectedItem()) && (choice.getSelectedItem()).equals(b[1])) {
+					daylist.add(Integer.parseInt(b[2]));
+
+				}
+			}
+			Integer[] daylist2 = daylist.toArray(new Integer[daylist.size()]);
+			frame.endday.removeAll();
+			for (int j = 0; j < daylist2.length; j++) {
+				frame.endday.add(Integer.toString(daylist2[j]));
+			}
+		}
+		Choice choice = (Choice) e.getSource();
+
+		// System.out.println(choice.getSelectedIndex());
+		// System.out.println(choice.getSelectedItem());
 
 	}
 
