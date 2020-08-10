711fe921b5cbf91ee4d7d172ab3cdbd705b03d81
dbdmsdn10 <dbdmsdn10@gmail.com>
2020-08-10 17:57:53 +0900

    프로젝트 1일차
    음식 삽입, 검색, 먹었던거 확인 및 로그인등
    
    해야할일 삽입후 새로고침
    웹검색 페이지 버튼

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -4,24 +4,9 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]) {
-		int[][] a = new int[2][41];
-		a[0][0]=1; 
-		a[0][1]=0; 
-		a[1][0]=0;
-		a[1][1]=1;
-		for(int i=2;i<41;i++)
-		{
-			a[0][i]=a[0][i-1]+a[0][i-2];
-			a[1][i]=a[1][i-1]+a[1][i-2];
-		}
+		String a="'";
 		
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
-		for(int i=0;i<num;i++)
-		{
-			int b = scan.nextInt();
-			System.out.println(a[0][b]+" "+a[1][b]);
-		}
+		System.out.println(a);
 		
 	}
 }
